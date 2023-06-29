185f4ddac2922208e77f1e94d309132f73d0ec96
megaphieu <megaphieu@gmail.com>
2023-06-29 17:22:54 +0700

    try Scratch app

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -17,16 +17,41 @@ export default function FirstPost() {
           console.log(`script loaded correctly, window.FB has been populated`)
         }
       />
+
       <h1>First Post</h1>
-      <Image
-        src="/images/truedumage.PNG"
-        height={144}
-        width={144}
-        alt="truedumage"
-      />
-      <h2>
-        <Link href={"/"}>← Back to home</Link>
-      </h2>
+      <p>
+        My first scratch app. You can also find me on{" "}
+        <Link href={"https://scratch.mit.edu/users/megaphieu/"}>
+          this site!
+        </Link>{" "}
+      </p>
+      <div>
+        <iframe
+          src="https://scratch.mit.edu/projects/870461663/embed"
+          allowtransparency="true"
+          width="485"
+          height="402"
+          frameborder="0"
+          scrolling="no"
+          allowfullscreen
+        ></iframe>
+      </div>
+
+      <div>
+        <p>Phản ứng của tôi kiểu:</p>
+        <Image
+          src="/images/truedumage.PNG"
+          height={144}
+          width={144}
+          alt="truedumage"
+        />
+      </div>
+
+      <div>
+        <h2>
+          <Link href={"/"}>← Back to home</Link>
+        </h2>
+      </div>
     </Layout>
   );
 }
